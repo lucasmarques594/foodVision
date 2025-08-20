2075bc249fc269f34de9cf1059ab3059e4c8b3f4
Lucas Marques <demordevil594@gmail.com>
2025-08-19 22:30:23 -0300

    feat(repository): add IRecipeRepository interface with basic CRUD operations

---
+++ src/application/repositories/IRecipeRepository.ts
@@ -0,0 +1,8 @@
+import { Recipe } from '../../domain/entities/Recipe';
+
+export interface IRecipeRepository {
+  save(recipe: Recipe): Promise<Recipe>;
+  findAll(): Promise<Recipe[]>;
+  findById(id: string): Promise<Recipe | null>;
+  delete(id: string): Promise<void>;
+}
